197dc411c3858da28e55d0530ae63a7878132fc4
Nick Groszewski <groszewn@gmail.com>
2024-02-24 10:18:22 -0500

    Hard refresh

--- external/versions.tf
+++ external/versions.tf
@@ -3,7 +3,7 @@ terraform {
 
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "khuedoan"
+    organization = "groszewski"
 
     workspaces {
       name = "homelab-external"
